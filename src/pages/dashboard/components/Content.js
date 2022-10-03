ea1a1a953bdb8749ea607f475b026708a2135187
iostream-code <aryakmifjannah2603@gmail.com>
2022-10-04 01:08:55 +0700

    disable typography

--- src/pages/dashboard/components/Content.js
+++ src/pages/dashboard/components/Content.js
@@ -19,7 +19,7 @@ import TableRow from '@mui/material/TableRow';
 
 
 function createData(name, calories, fat, carbs, protein) {
-return { name, calories, fat, carbs, protein };
+    return { name, calories, fat, carbs, protein };
 }
 
 const rows = [
@@ -34,77 +34,76 @@ const Content = () => {
     return (
         <div>
             <Paper sx={{ maxWidth: 936, margin: 'auto', overflow: 'hidden' }}>
-                    <AppBar
-                        position="static"
-                        color="default"
-                        elevation={0}
-                        sx={{ borderBottom: '1px solid rgba(0, 0, 0, 0.12)' }}
-                    >
-                        <Toolbar>
-                            <Grid container spacing={2} alignItems="center">
-                                <Grid item>
-                                    <SearchIcon color="inherit" sx={{ display: 'block' }} />
-                                </Grid>
-                                <Grid item xs>
-                                    <TextField
-                                        fullWidth
-                                        placeholder="Search by email address, phone number, or user UID"
-                                        InputProps={{
-                                            disableUnderline: true,
-                                            sx: { fontSize: 'default' },
-                                        }}
-                                        variant="standard"
-                                    />
-                                </Grid>
-                                <Grid item>
-                                    <Button variant="contained" sx={{ mr: 1 }}>
-                                        Add user
-                                    </Button>
-                                    <Tooltip title="Reload">
-                                        <IconButton>
-                                            <RefreshIcon color="inherit" sx={{ display: 'block' }} />
-                                        </IconButton>
-                                    </Tooltip>
-                                </Grid>
+                <AppBar
+                    position="static"
+                    color="default"
+                    elevation={0}
+                    sx={{ borderBottom: '1px solid rgba(0, 0, 0, 0.12)' }}
+                >
+                    <Toolbar>
+                        <Grid container spacing={2} alignItems="center">
+                            <Grid item>
+                                <SearchIcon color="inherit" sx={{ display: 'block' }} />
                             </Grid>
-                        </Toolbar>
-                        <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Dessert (100g serving)</TableCell>
-                            <TableCell align="right">Calories</TableCell>
-                            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map((row) => (
-                            <TableRow
-                            key={row.name}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                            <TableCell component="th" scope="row">
-                                {row.name}
-                            </TableCell>
-                            <TableCell align="right">{row.calories}</TableCell>
-                            <TableCell align="right">{row.fat}</TableCell>
-                            <TableCell align="right">{row.carbs}</TableCell>
-                            <TableCell align="right">{row.protein}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-                    </AppBar>
-                    <Typography sx={{ my: 5, mx: 2 }} color="text.secondary" align="center">
-                        No users for this project yet
-                    </Typography>
+                            <Grid item xs>
+                                <TextField
+                                    fullWidth
+                                    placeholder="Search by email address, phone number, or user UID"
+                                    InputProps={{
+                                        disableUnderline: true,
+                                        sx: { fontSize: 'default' },
+                                    }}
+                                    variant="standard"
+                                />
+                            </Grid>
+                            <Grid item>
+                                <Button variant="contained" sx={{ mr: 1 }}>
+                                    Add user
+                                </Button>
+                                <Tooltip title="Reload">
+                                    <IconButton>
+                                        <RefreshIcon color="inherit" sx={{ display: 'block' }} />
+                                    </IconButton>
+                                </Tooltip>
+                            </Grid>
+                        </Grid>
+                    </Toolbar>
+                    <TableContainer component={Paper}>
+                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Customer Name</TableCell>
+                                    <TableCell align="right">Email</TableCell>
+                                    <TableCell align="right">Address</TableCell>
+                                    <TableCell align="right">Client</TableCell>
+                                    <TableCell align="right">ID</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {rows.map((row) => (
+                                    <TableRow
+                                        key={row.name}
+                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                    >
+                                        <TableCell component="th" scope="row">
+                                            {row.name}
+                                        </TableCell>
+                                        <TableCell align="right">{row.calories}</TableCell>
+                                        <TableCell align="right">{row.fat}</TableCell>
+                                        <TableCell align="right">{row.carbs}</TableCell>
+                                        <TableCell align="right">{row.protein}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </AppBar>
+                {/* <Typography sx={{ my: 5, mx: 2 }} color="text.secondary" align="center">
+                    No users for this project yet
+                </Typography> */}
             </Paper>
         </div>
-        
-);
+    );
 }
 
 export default Content
