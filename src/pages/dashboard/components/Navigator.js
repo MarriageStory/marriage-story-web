b497f617f7839da1b3ba7926876bb98ebc07b251
Febri Permata <emiliana.febri@gmail.com>
2022-10-10 14:12:15 +0700

    tes commit

--- src/pages/dashboard/components/Navigator.js
+++ src/pages/dashboard/components/Navigator.js
@@ -9,20 +9,16 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import PeopleIcon from '@mui/icons-material/People';
+import HomeIcon from '@mui/icons-material/Home';
 import DnsRoundedIcon from '@mui/icons-material/DnsRounded';
-import PublicIcon from '@mui/icons-material/Public';
-import SettingsIcon from '@mui/icons-material/Settings';
 import { AccountCircle } from '@mui/icons-material';
 
 const categories = [
     {
-        id: 'Action',
+        id: 'Menu',
         children: [
-            { id: 'Profile', icon: <PeopleIcon /> },
-            { id: 'Client', icon: <DnsRoundedIcon /> },
-            { id: 'Setting', icon: <SettingsIcon /> },
-            { id: 'Analytics', icon: <PublicIcon /> },
+            { id: 'Home', icon: <HomeIcon /> },
+            { id: 'Clients', icon: <DnsRoundedIcon /> },
             { id: 'Signout', icon: <AccountCircle /> },
         ],
     },
@@ -58,16 +54,9 @@ export default function Navigator(props) {
     return (
         <Drawer variant="permanent" {...other}>
             <List disablePadding>
-                <ListItemButton sx={{ ...item, ...itemCategory, fontSize: 22, color: '#fff' }}>
-                    <img src="../../../../public/assets/img/Title.png" alt="Marriage Story" />
-                </ListItemButton>
-                {/* <ListItemButton component="a" href="#">
-                    <ListItemIcon sx={{ fontSize: 20 }}>🔥</ListItemIcon>
-                    <ListItemText
-                        sx={{ my: 0, fontSize: 80}}
-                        primary="Marriage Story"
-                    />
-                </ListItemButton> */}
+                <ListItem sx={{ ...item, ...itemCategory, fontSize: 22, color: '#fff' }}>
+                    <img src="../assets/marriagelogo.png" alt="Marriage Story" />
+                </ListItem>
                 {categories.map(({ id, children }) => (
                     <Box key={id} sx={{ bgcolor: '#FBC0C0' }}>
                         <ListItem sx={{ py: 2, px: 3 }}>
